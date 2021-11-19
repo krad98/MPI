3d0df3db99f14f88aab8c4b48529a9815bc93b85
krad98 <49642965+krad98@users.noreply.github.com>
2021-11-19 14:11:30 +0100

    Add files via upload

---
+++ somme.c
@@ -0,0 +1,53 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+
+
+int somme(int n);
+
+int main(int argc, char* argv[])
+{	
+	
+	long clk_tck = CLOCKS_PER_SEC;
+	clock_t start, end;
+
+	int tab[2] = {1, 2};
+	int i, x, j;
+
+
+	start = clock();
+
+	for (i = 0; i < 2; i++)
+	{
+		x = tab[i];
+
+		tab[i] = somme(x);
+	}
+
+	end = clock();
+
+	for (j = 0; j < 2; j++)
+	{
+		printf("%d\n", tab[j]);
+	}
+
+	printf("Temps consommer (s) : %lf\n", (double)(end-start)/(double)clk_tck);
+
+
+	return 0;
+}
+
+
+
+int somme(int n)
+{
+	int x = 0;
+    for (int i = 0; i < 1000000000; i++)
+    {
+   
+    	x = x + n;
+
+    }
+    return x;
+}
---
+++ somme_parallel.c
@@ -0,0 +1,71 @@
+#include <mpi.h>
+#include <stdio.h>
+#include <stdlib.h>
+
+int somme(int n);
+
+int main(int argc, char **argv) {
+    double start, end;
+    int size, rank, n = 2;
+
+    MPI_Init(&argc, &argv);
+    MPI_Comm_size(MPI_COMM_WORLD, &size);
+    MPI_Comm_rank(MPI_COMM_WORLD, &rank);
+
+    int *globaldata=NULL;
+    int localdata;
+
+    if (rank == 0) {
+        globaldata = malloc(n * sizeof(int) );
+        for (int i=0; i<n; i++)
+            globaldata[i] = i+1;
+
+        printf("Processor %d has data: ", rank);
+        for (int i=0; i<n; i++)
+            printf("%d ", globaldata[i]);
+        printf("\n");
+    }
+
+    start = MPI_Wtime();
+
+    MPI_Scatter(globaldata, 1, MPI_INT, &localdata, 1, MPI_INT, 0, MPI_COMM_WORLD);
+
+    printf("Processor %d has data %d\n", rank, localdata);
+    int a = localdata;
+    localdata = somme(a);
+    printf("Processor %d multiplied the data, now has %d\n", rank, localdata);
+
+    MPI_Gather(&localdata, 1, MPI_INT, globaldata, 1, MPI_INT, 0, MPI_COMM_WORLD);
+
+    end = MPI_Wtime();
+
+    if (rank == 0)
+    {
+        printf("Processor %d has data: ", rank);
+        for (int i=0; i<n; i++)
+            printf("%d ", globaldata[i]);
+        printf("\n");
+
+        printf("\nTemps: %f sec\n", end-start);
+
+    }
+
+
+    if (rank == 0)
+        free(globaldata);
+
+    MPI_Finalize();
+    return 0;
+}
+
+int somme(int n)
+{
+    int x = 0;
+    for (int i = 0; i < 1000000000; ++i)
+    {
+   
+        x = x + n;
+
+    }
+    return x;
+}
